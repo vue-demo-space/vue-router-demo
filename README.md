5d430dbd908ac7d3afa46aa9efa993a9be55ad65
hanzichi <hanzichi@outlook.com>
2019-03-13 21:51:00 +0800

    导航解析流程

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
 # vue-router-demo
 
-## Project setup
-```
-npm install
-```
+完整的导航解析流程：
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. 导航被触发
+2. 在失活的组件里（即将离开的页面组件）调用离开守卫 beforeRouteLeave
+3. 调用全局前置守卫 beforeEach
+4. 在重用的组件里调用 beforeRouteUpdate
+5. 在路由配置里调用 beforeEnter（和 4 并级）
+6. 解析异步路由组件
+7. 在被激活的组件里（即将进入的页面组件）调用 beforeRouteEnter
+8. 调用全局的解析守卫 [beforeResolve](https://router.vuejs.org/zh/guide/advanced/navigation-guards.html#%E5%85%A8%E5%B1%80%E8%A7%A3%E6%9E%90%E5%AE%88%E5%8D%AB)（导航被确认前触发）
+9. 导航被确认
+10. 调用全局后置守卫 afterEach
+11. 触发 DOM 更新
+12. 用创建好的实例调用 beforeRouteEnter 守卫中传给 next 的回调函数
--- src/router.js
+++ src/router.js
@@ -4,14 +4,19 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      beforeEnter: (to, from, next) => {
+        // ...
+        console.log('路由独享的守卫 - 定义在路由上的 beforeEnter')
+        next()
+      }
     },
     {
       path: '/about',
@@ -20,6 +25,30 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/argu',
+      name: 'argu',
+      component: () => import(/* webpackChunkName: "argu" */ './views/Argu.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // 可以做一些登录逻辑的处理
+  console.log('全局前置守卫 - beforeEach')
+  next()
+})
+
+router.afterEach((to, from) => {
+  // ...
+  console.log('全局后置钩子 - afterEach')
+})
+
+router.beforeResolve((to, from, next) => {
+  // ...
+  console.log('全局解析守卫 - beforeResolve')
+  next()
+})
+
+export default router
